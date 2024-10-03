64401d3737df412e6ab06fbe6fd26d1be073914a
Henry Perkins <htperkins@gmail.com>
2024-10-03 17:58:31 -0500

    Update acorn_inserter.js

--- scripts/acorn_inserter.js
+++ scripts/acorn_inserter.js
@@ -1,139 +1,108 @@
 // acorn_inserter.js
 
-const acorn = require('acorn');
-const acornWalk = require('acorn-walk');
-const acornTS = require('acorn-typescript');
-const astring = require('astring');
 const fs = require('fs');
+const { parse } = require('@typescript-eslint/typescript-estree');
+const traverse = require('@babel/traverse').default;
+const generate = require('@babel/generator').default;
 
-// Read data from stdin
 let inputChunks = [];
 process.stdin.on('data', chunk => {
-    inputChunks.push(chunk);
+  inputChunks.push(chunk);
 });
 
 process.stdin.on('end', () => {
-    const inputData = JSON.parse(inputChunks.join(''));
-    const { code, documentation, language } = inputData;
+  const inputData = JSON.parse(inputChunks.join(''));
+  const { code, documentation, language } = inputData;
 
-    let parserOptions = {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        locations: true,
-        ranges: true,
-        onComment: []
+  // Parse the code
+  let ast;
+  try {
+    ast = parse(code, {
+      loc: true,
+      range: true,
+      comment: true,
+      tokens: true,
+      errorOnUnknownASTType: false,
+      jsx: true,
+    });
+  } catch (e) {
+    console.error(`Parsing error: ${e.message}`);
+    process.exit(1);
+  }
+
+  // Build a map of function and class names to their docstrings
+  const docstringsMapping = {};
+
+  // Process functions
+  if (documentation.functions) {
+    documentation.functions.forEach(funcDoc => {
+      if (funcDoc.name && funcDoc.docstring) {
+        docstringsMapping[funcDoc.name] = funcDoc.docstring;
+      }
+    });
+  }
+
+  // Process classes and their methods
+  if (documentation.classes) {
+    documentation.classes.forEach(classDoc => {
+      if (classDoc.name && classDoc.docstring) {
+        docstringsMapping[classDoc.name] = classDoc.docstring;
+      }
+      if (classDoc.methods) {
+        classDoc.methods.forEach(methodDoc => {
+          if (methodDoc.name && methodDoc.docstring) {
+            const fullName = `${classDoc.name}.${methodDoc.name}`;
+            docstringsMapping[fullName] = methodDoc.docstring;
+          }
+        });
+      }
+    });
+  }
+
+  // Function to insert docstrings as comments
+  function insertDocstring(node, docstring) {
+    const comment = {
+      type: 'Block',
+      value: `*\n * ${docstring.replace(/\n/g, '\n * ')}\n `,
     };
-
-    // Use Acorn extended with TypeScript if language is TypeScript
-    let Parser;
-    if (language === 'typescript') {
-        Parser = acorn.Parser.extend(acornTS());
-    } else {
-        Parser = acorn.Parser;
+    if (!node.leadingComments) {
+      node.leadingComments = [];
     }
+    node.leadingComments.push(comment);
+  }
 
-    // Parse the code
-    let comments = [];
-    let ast;
-    try {
-        ast = Parser.parse(code, {
-            ...parserOptions,
-            onComment: comments
-        });
-    } catch (e) {
-        console.error(`Acorn parsing error: ${e.message}`);
-        process.exit(1);
-    }
-
-    // Attach comments to nodes
-    acorn.addComments(ast, comments);
-
-    // Build a map of function and class names to their docstrings
-    const docstringsMapping = {};
-
-    // Process functions
-    if (documentation.functions) {
-        documentation.functions.forEach(funcDoc => {
-            if (funcDoc.name && funcDoc.docstring) {
-                docstringsMapping[funcDoc.name] = funcDoc.docstring;
-            }
-        });
-    }
-
-    // Process classes and their methods
-    if (documentation.classes) {
-        documentation.classes.forEach(classDoc => {
-            if (classDoc.name && classDoc.docstring) {
-                docstringsMapping[classDoc.name] = classDoc.docstring;
-            }
-            if (classDoc.methods) {
-                classDoc.methods.forEach(methodDoc => {
-                    if (methodDoc.name && methodDoc.docstring) {
-                        const fullName = `${classDoc.name}.${methodDoc.name}`;
-                        docstringsMapping[fullName] = methodDoc.docstring;
-                    }
-                });
-            }
-        });
-    }
-
-    // Function to insert docstrings as comments
-    function insertDocstring(node, docstring) {
-        const commentNode = {
-            type: 'Block',
-            value: `*\n * ${docstring.replace(/\n/g, '\n * ')}\n `,
-            start: node.start,
-            end: node.start,
-            loc: node.loc,
-            range: [node.start, node.start]
-        };
-        node.leadingComments = node.leadingComments || [];
-        node.leadingComments.push(commentNode);
-    }
-
-    // Traverse the AST and insert docstrings
-    acornWalk.simple(ast, {
-        FunctionDeclaration(node) {
-            const name = node.id ? node.id.name : 'anonymous';
-            if (docstringsMapping[name]) {
-                insertDocstring(node, docstringsMapping[name]);
-            }
-        },
-        VariableDeclaration(node) {
-            node.declarations.forEach(declarator => {
-                if (
-                    declarator.init &&
-                    (declarator.init.type === 'FunctionExpression' || declarator.init.type === 'ArrowFunctionExpression')
-                ) {
-                    const name = declarator.id.name;
-                    if (docstringsMapping[name]) {
-                        insertDocstring(declarator, docstringsMapping[name]);
-                    }
-                }
-            });
-        },
-        ClassDeclaration(node) {
-            const className = node.id.name;
-            if (docstringsMapping[className]) {
-                insertDocstring(node, docstringsMapping[className]);
-            }
-            node.body.body.forEach(element => {
-                if (element.type === 'MethodDefinition') {
-                    const methodName = element.key.name;
-                    const fullName = `${className}.${methodName}`;
-                    if (docstringsMapping[fullName]) {
-                        insertDocstring(element, docstringsMapping[fullName]);
-                    }
-                }
-            });
+  // Traverse the AST and insert docstrings
+  traverse(ast, {
+    enter(path) {
+      const node = path.node;
+      if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression') {
+        const name = node.id ? node.id.name : 'anonymous';
+        if (docstringsMapping[name]) {
+          insertDocstring(node, docstringsMapping[name]);
         }
-    });
+      } else if (node.type === 'ClassDeclaration') {
+        const className = node.id ? node.id.name : 'anonymous';
+        if (docstringsMapping[className]) {
+          insertDocstring(node, docstringsMapping[className]);
+        }
+        if (node.body && node.body.body) {
+          node.body.body.forEach(element => {
+            if (element.type === 'MethodDefinition') {
+              const methodName = element.key.name;
+              const fullName = `${className}.${methodName}`;
+              if (docstringsMapping[fullName]) {
+                insertDocstring(element, docstringsMapping[fullName]);
+              }
+            }
+          });
+        }
+      }
+    },
+  });
 
-    // Generate code from modified AST, including comments
-    const modifiedCode = astring.generate(ast, {
-        comments: true
-    });
+  // Generate code from modified AST, including comments
+  const { code: modifiedCode } = generate(ast, { comments: true });
 
-    // Output the modified code
-    console.log(modifiedCode);
+  // Output the modified code
+  console.log(modifiedCode);
 });
